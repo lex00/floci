37b9fe99b046f07b85875fa8b2c5965c4dd9dded
Leandro-ft <leandro@fintree.cl>
2026-07-13 18:22:28 -0400

    fix(ec2): apply cidr-block filter in DescribeSubnets DescribeSubnets ignored Name=cidr-block and returned all subnets in the VPC. Match AWS aliases cidr-block, cidrBlock, and cidr. Fixes #1643

--- src/main/java/io/github/hectorvent/floci/services/ec2/Ec2Service.java
+++ src/main/java/io/github/hectorvent/floci/services/ec2/Ec2Service.java
@@ -2720,6 +2720,7 @@ public class Ec2Service implements ContainerTeardown {
                 case "vpc-id" -> matchesValue(values, subnet.getVpcId());
                 case "state" -> matchesValue(values, subnet.getState());
                 case "availabilityZone", "availability-zone" -> matchesValue(values, subnet.getAvailabilityZone());
+                case "cidr-block", "cidrBlock", "cidr" -> matchesValue(values, subnet.getCidrBlock());
                 default -> true;
             };
         }
