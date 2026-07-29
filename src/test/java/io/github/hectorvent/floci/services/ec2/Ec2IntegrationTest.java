81e5fa183cfba647262e53932f352137de7d6d59
Leandro-ft <leandro@fintree.cl>
2026-07-28 22:24:01 -0400

    fix(ec2): apply cidr-block filter in DescribeSubnets (#1864)
    * fix(ec2): apply cidr-block filter in DescribeSubnets
    DescribeSubnets ignored Name=cidr-block and returned all subnets
    in the VPC. Match AWS aliases cidr-block, cidrBlock, and cidr.
    Fixes #1643
    
    * test(ec2): set describeSubnetsByCidrBlockFilter Order to 23
    
    (cherry picked from commit 8f3597c2052a8efc63992f6900706f804091b1bb)

--- src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
+++ src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
@@ -751,6 +751,57 @@ class Ec2IntegrationTest {
     }
 
     @Test
+    @Order(23)
+    void describeSubnetsByCidrBlockFilter() {
+        String isolatedVpcId = given()
+            .formParam("Action", "CreateVpc")
+            .formParam("CidrBlock", "10.10.0.0/16")
+            .header("Authorization", AUTH_HEADER)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(200)
+            .extract().path("CreateVpcResponse.vpc.vpcId");
+
+        String firstSubnetId = given()
+            .formParam("Action", "CreateSubnet")
+            .formParam("VpcId", isolatedVpcId)
+            .formParam("CidrBlock", "10.10.1.0/24")
+            .formParam("AvailabilityZone", "us-east-1a")
+            .header("Authorization", AUTH_HEADER)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(200)
+            .extract().path("CreateSubnetResponse.subnet.subnetId");
+
+        given()
+            .formParam("Action", "CreateSubnet")
+            .formParam("VpcId", isolatedVpcId)
+            .formParam("CidrBlock", "10.10.2.0/24")
+            .formParam("AvailabilityZone", "us-east-1b")
+            .header("Authorization", AUTH_HEADER)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(200);
+
+        given()
+            .formParam("Action", "DescribeSubnets")
+            .formParam("Filter.1.Name", "vpc-id")
+            .formParam("Filter.1.Value.1", isolatedVpcId)
+            .formParam("Filter.2.Name", "cidr-block")
+            .formParam("Filter.2.Value.1", "10.10.1.0/24")
+            .header("Authorization", AUTH_HEADER)
+        .when()
+            .post("/")
+        .then()
+            .statusCode(200)
+            .body("DescribeSubnetsResponse.subnetSet.item.subnetId", equalTo(firstSubnetId))
+            .body("DescribeSubnetsResponse.subnetSet.item.cidrBlock", equalTo("10.10.1.0/24"));
+    }
+
+    @Test
     @Order(21)
     void createSubnetHasAssignIpv6AddressOnCreation() {
         given()
